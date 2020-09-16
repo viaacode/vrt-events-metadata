ff84b3ba6b00249b8992430dcf66363ef9596cd7
Maarten <maartendeschrijver@gmail.com>
2020-09-16 15:47:49 +0200

    DEV-1216 Add test for `construct_sidecar` fn

--- tests/helpers/test_xml_helper.py
+++ tests/helpers/test_xml_helper.py
@@ -1,7 +1,7 @@
 import pytest
 
 from lxml import etree
-from app.helpers.xml_helper import transform_to_ebucore
+from app.helpers.xml_helper import transform_to_ebucore, construct_sidecar
 from tests.resources import resources
 
 
@@ -14,3 +14,17 @@ def test_ebucore_transformation():
 
     # ASSERT
     assert b"ebuCoreMain" in ebu_xml
+
+def test_construct_sidecar():
+    # ARRANGE
+    metadata_dict = {
+        "PID": "abc123",
+        "Md5": "a1b2c3d4e5f6"
+    }
+    ref_xml = b"<?xml version='1.0' encoding='UTF-8'?>\n<MediaHAVEN_external_metadata>\n  <title>Collateral: pid: abc123</title>\n  <description>Metadata for essence:\n    - PID: abc123\n    - CP: VRT\n    </description>\n  <MDProperties>\n    <CP>VRT</CP>\n    <CP_id>OR-rf5kf25</CP_id>\n    <sp_name>s3</sp_name>\n    <PID>abc123_metadata</PID>\n    <md5>a1b2c3d4e5f6</md5>\n    <dc_relations>\n      <is_verwant_aan>abc123</is_verwant_aan>\n    </dc_relations>\n  </MDProperties>\n</MediaHAVEN_external_metadata>\n"
+
+    # ACT
+    test_xml = construct_sidecar(metadata_dict)
+
+    # ASSERT
+    assert ref_xml == test_xml
