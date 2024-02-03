d1a3094be263d894687abf74b3af77ab1fcedfa5
Mattias.Poppe <poppe.mattias@gmail.com>
2024-02-03 21:58:50 +0100

    DEV-2475 Add transformation test
    Test the output of the metadata transformation.

--- tests/test_app.py
+++ tests/test_app.py
@@ -5,6 +5,7 @@ from mediahaven.mocks.base_resource import MediaHavenPageObjectJSONMock
 from tests.resources import resources
 from tests.resources.mocks import mock_rabbit, mock_mediahaven
 from app.app import EventListener
+from app.helpers.events_parser import EventParser
 
 
 @pytest.fixture
@@ -29,3 +30,21 @@ def test_get_fragment(event_listener):
         fragment.Internal.FragmentId
         == "4885061ab2e047728558d24411dd44b8d89c983031994cec9d774270fb807f9697c9ac524f1a471da54c731ceac09bb0"
     )
+
+
+def test_transform_metadata(event_listener):
+    # ARRANGE
+    xml = resources.load_xml_resource("getMetadataResponse2")
+    event_parser = EventParser()
+    event = event_parser.get_event("getMetadataResponse", xml)
+    el = EventListener()
+
+    # ACT
+    transformed_xml = el._transform_metadata(event)
+
+    # ASSERT
+    expected_xml = resources.load_xml_resource(
+        "mhs_sidecar_getMetadataResponse2"
+    ).decode("utf-8")
+
+    assert transformed_xml == expected_xml
